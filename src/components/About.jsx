f8921afe7511b0a2027632b125643337ed0fd7bb
okean8901 <trangialoc892001@gmail.com>
2026-04-01 13:12:15 +0700

    fix linkedin vn to eng

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export function About() {
           <div className="reveal" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', maxWidth: '100%' }}>
             <div
               className="badge-base LI-profile-badge"
-              data-locale="vi_VN"
+              data-locale="en_US"
               data-size="medium"
               data-theme="dark"
               data-type="VERTICAL"
